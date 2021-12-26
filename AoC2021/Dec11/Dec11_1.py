cb1ef1d1117b092b4dd8c27c39732ac44b86b7ae
rbaerheim <rgbhm@hotmail.com>
2021-12-26 23:22:40 +0100

    Dec 11 part 1 done

--- AoC2021/Dec11/Dec11_1.py
+++ AoC2021/Dec11/Dec11_1.py
@@ -1,6 +1,7 @@
 import numpy as np
+from collections import deque
 
-inputs = []
+inputs = deque()
 
 with open("input.txt", "r") as input:
     for line in input:
@@ -13,18 +14,131 @@ input.close
 class Dumbo:
     def __init__(self, array):
         self.array = array
-        self.flashList = []
+        self.flashList = deque()
+        self.flashCount = 0
 
-    def increase(self):
+    def increaseAllOne(self):
         for r in range(len(self.array)):
             for c in range(len(self.array[r])):
-                self.array[r][c] += 2
+                self.array[r][c] += 1
                 if self.array[r][c] == 10:
+                    self.flashCount += 1
                     self.flashList.append((r,c))
                     self.array[r][c] = 0
 
     def checkAdjacent(self):
-        pass
+        while len(self.flashList) > 0:
+            coord = self.flashList.pop()
+
+            # Left
+            try:
+                if coord[1] > 0:
+                    if self.array[coord[0]][coord[1] - 1] != 0:
+                        self.array[coord[0]][coord[1] - 1] += 1
+                    if self.array[coord[0]][coord[1] - 1] == 10:
+                        self.flashCount += 1
+                        self.flashList.appendleft((coord[0], coord[1] - 1))
+                        self.array[coord[0]][coord[1] - 1] = 0
+            except IndexError:
+                pass
+
+            # Right
+            try:
+                if self.array[coord[0]][coord[1] + 1] != 0:
+                    self.array[coord[0]][coord[1] + 1] += 1
+                if self.array[coord[0]][coord[1] + 1] == 10:
+                    self.flashCount += 1
+                    self.flashList.appendleft((coord[0], coord[1] + 1))
+                    self.array[coord[0]][coord[1] + 1] = 0
+            except IndexError:
+                pass
+
+            # Top
+            try:
+                if (coord[0] - 1) < 0:
+                    pass
+                else:
+                    if self.array[coord[0] - 1][coord[1]] != 0:
+                        self.array[coord[0] - 1][coord[1]] += 1
+                    if self.array[coord[0] - 1][coord[1]] == 10:
+                        self.flashCount += 1
+                        self.flashList.appendleft((coord[0] - 1, coord[1]))
+                        self.array[coord[0] - 1][coord[1]] = 0
+            except IndexError:
+                pass
+
+            # Bottom
+            try:
+                if (coord[0] + 1) > 9:
+                    pass
+                else:
+                    if self.array[coord[0] + 1][coord[1]] != 0:
+                        self.array[coord[0] + 1][coord[1]] += 1
+                    if self.array[coord[0] + 1][coord[1]] == 10:
+                        self.flashCount += 1
+                        self.flashList.appendleft((coord[0] + 1, coord[1]))
+                        self.array[coord[0] + 1][coord[1]] = 0
+            except IndexError:
+                pass
+
+            # Lefttop
+            try:
+                if (coord[0] - 1) < 0:
+                    pass
+                else:
+                    if coord[1] > 0:
+                        if self.array[coord[0] - 1][coord[1] - 1] != 0:
+                            self.array[coord[0] - 1][coord[1] - 1] += 1
+                        if self.array[coord[0] - 1][coord[1] - 1] == 10:
+                            self.flashCount += 1
+                            self.flashList.appendleft((coord[0] - 1, coord[1] - 1))
+                            self.array[coord[0] - 1][coord[1] - 1] = 0
+            except IndexError:
+                pass
+
+            # Righttop
+            try:
+                if (coord[0] - 1) < 0:
+                    pass
+                else:
+                    if self.array[coord[0] - 1][coord[1] + 1] != 0:
+                        self.array[coord[0] - 1][coord[1] + 1] += 1
+                    if self.array[coord[0] - 1][coord[1] + 1] == 10:
+                        self.flashCount += 1
+                        self.flashList.appendleft((coord[0] - 1, coord[1] + 1))
+                        self.array[coord[0] - 1][coord[1] + 1] = 0
+            except IndexError:
+                pass
+
+            # Leftbottom
+            try:
+                if (coord[0] + 1) > 9:
+                    pass
+                else:
+                    if coord[1] > 0:
+                        if self.array[coord[0] + 1][coord[1] - 1] != 0:
+                            self.array[coord[0] + 1][coord[1] - 1] += 1
+                        if self.array[coord[0] + 1][coord[1] - 1] == 10:
+                            self.flashCount += 1
+                            self.flashList.appendleft((coord[0] + 1, coord[1] - 1))
+                            self.array[coord[0] + 1][coord[1] - 1] = 0
+            except IndexError:
+                pass
+
+            # Rightbottom
+            try:
+                if (coord[0] + 1) > 9:
+                    pass
+                else:
+                    if self.array[coord[0] + 1][coord[1] + 1] != 0:
+                        self.array[coord[0] + 1][coord[1] + 1] += 1
+                    if self.array[coord[0] + 1][coord[1] + 1] == 10:
+                        self.flashCount += 1
+                        self.flashList.appendleft((coord[0] + 1, coord[1] + 1))
+                        self.array[coord[0] + 1][coord[1] + 1] = 0
+            except IndexError:
+                pass
+            
 
     def __repr__(self):
         return f"{self.array}"
@@ -33,7 +147,7 @@ class Dumbo:
 
 if __name__ == "__main__":
     dumbo = Dumbo(theArray)
-    print(dumbo)
-    dumbo.increase()
-    print(dumbo)
-    print(dumbo.flashList)
+    for _ in range(100):
+        dumbo.increaseAllOne()
+        dumbo.checkAdjacent()
+    print(f"Part 1: {dumbo.flashCount}")
