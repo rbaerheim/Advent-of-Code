460c896fc80893269290c02e145f5c5ffd9b18fe
rbaerheim <rgbhm@hotmail.com>
2022-12-01 18:58:46 +0100

    Added C# solution to dec1 2022 and added gitignore

---
+++ C#AoC2022/Dec1/Dec1_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Diagnostics;
+
+var watch = new Stopwatch();
+watch.Start();
+
+var path = @"input.txt";
+
+var enumLines = File.ReadLines(path, Encoding.UTF8);
+
+int temp = 0;
+int max = 0;
+
+foreach (string line in enumLines)
+{
+    line.Trim();
+    if (string.IsNullOrEmpty(line))
+    {
+        if (temp > max)
+        {
+            max = temp;
+        }
+        temp = 0;
+        continue;
+    }
+    temp += Int32.Parse(line);
+}
+
+Console.WriteLine(max); // 69626
+
+watch.Stop();
+Console.WriteLine(watch.ElapsedMilliseconds); // 25ms
---
+++ C#AoC2022/Dec1/Dec1_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Diagnostics;
+
+var watch = new Stopwatch();
+watch.Start();
+
+var path = @"input.txt";
+
+var enumLines = File.ReadLines(path, Encoding.UTF8);
+
+int[] maxArray = new int[3];
+int temp = 0;
+
+foreach (string line in enumLines)
+{
+    line.Trim();
+    if (string.IsNullOrEmpty(line))
+    {
+        for (int i = 0; i < maxArray.Length; i++)
+        {
+            if (temp > maxArray[i])
+            {
+                maxArray[i] = temp;
+                Array.Sort(maxArray);
+                break;
+            }
+        }
+        temp = 0;
+        continue;
+    }
+    temp += Int32.Parse(line);
+}
+Console.WriteLine(maxArray.Sum()); // 206780
+
+watch.Stop();
+Console.WriteLine(watch.ElapsedMilliseconds); // 27ms
