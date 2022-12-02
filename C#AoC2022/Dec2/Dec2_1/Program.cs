5081a577b274892cae20a70d7dc33181701ab24c
rbaerheim <rgbhm@hotmail.com>
2022-12-02 09:28:18 +0100

    Added dec 2

---
+++ C#AoC2022/Dec2/Dec2_1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Text;
+using System.Diagnostics;
+
+var watch = new Stopwatch();
+watch.Start();
+
+var path = @"input.txt";
+
+var enumLines = File.ReadLines(path, Encoding.UTF8);
+
+var cities = new Dictionary<string, int>(){
+    {"A X", 0},
+    {"A Y", 0},
+    {"A Z", 0},
+    {"B X", 0},
+    {"B Y", 0},
+    {"B Z", 0},
+    {"C X", 0},
+    {"C Y", 0},
+    {"C Z", 0}
+};
+
+int totalScore = 0;
+
+foreach (string line in enumLines)
+{
+    line.Trim();
+    cities[line] += 1;
+}
+
+foreach (KeyValuePair<string, int> kvp in cities)
+{
+    if (kvp.Key == "A X")
+    {
+        totalScore += (kvp.Value * 4);
+    }
+    else if (kvp.Key == "A Y")
+    {
+        totalScore += (kvp.Value * 8);
+    }
+    else if (kvp.Key == "A Z")
+    {
+        totalScore += (kvp.Value * 3);
+    }
+    else if (kvp.Key == "B X")
+    {
+        totalScore += (kvp.Value * 1);
+    }
+    else if (kvp.Key == "B Y")
+    {
+        totalScore += (kvp.Value * 5);
+    }
+    else if (kvp.Key == "B Z")
+    {
+        totalScore += (kvp.Value * 9);
+    }
+    else if (kvp.Key == "C X")
+    {
+        totalScore += (kvp.Value * 7);
+    }
+    else if (kvp.Key == "C Y")
+    {
+        totalScore += (kvp.Value * 2);
+    }
+    else if (kvp.Key == "C Z")
+    {
+        totalScore += (kvp.Value * 6);
+    }
+}
+
+Console.WriteLine(totalScore); // 9651
+
+watch.Stop();
+Console.WriteLine(watch.ElapsedMilliseconds); // 15ms
---
+++ C#AoC2022/Dec2/Dec2_2/Program.cs
@@ -0,0 +1,121 @@
+﻿using System.Text.RegularExpressions;
+using System.Text;
+using System.Diagnostics;
+
+var watch = new Stopwatch();
+watch.Start();
+
+var path = @"input.txt";
+
+var enumLines = File.ReadLines(path, Encoding.UTF8);
+
+var cities = new Dictionary<string, int>(){
+    {"AX", 0},
+    {"AY", 0},
+    {"AZ", 0},
+    {"BX", 0},
+    {"BY", 0},
+    {"BZ", 0},
+    {"CX", 0},
+    {"CY", 0},
+    {"CZ", 0}
+};
+
+int totalScore = 0;
+
+foreach (string line in enumLines)
+{
+    string str = Regex.Replace(line, @"\s", "");
+
+    if (str.EndsWith("X"))
+    {
+        if (str.StartsWith("A"))
+        {
+            str = Regex.Replace(str, ".$", "Z");
+        }
+        else if (str.StartsWith("B"))
+        {
+            str = Regex.Replace(str, ".$", "X");
+        }
+        else
+        {
+            str = Regex.Replace(str, ".$", "Y");
+        }
+    }
+    else if (str.EndsWith("Y"))
+    {
+        if (str.StartsWith("A"))
+        {
+            str = Regex.Replace(str, ".$", "X");
+        }
+        else if (str.StartsWith("B"))
+        {
+            str = Regex.Replace(str, ".$", "Y");
+        }
+        else
+        {
+            str = Regex.Replace(str, ".$", "Z");
+        }
+    }
+    else
+    {
+        if (str.StartsWith("A"))
+        {
+            str = Regex.Replace(str, ".$", "Y");
+        }
+        else if (str.StartsWith("B"))
+        {
+            str = Regex.Replace(str, ".$", "Z");
+        }
+        else
+        {
+            str = Regex.Replace(str, ".$", "X");
+        }
+    }
+    cities[str] += 1;
+}
+
+foreach (KeyValuePair<string, int> kvp in cities)
+{
+    if (kvp.Key == "AX")
+    {
+        totalScore += (kvp.Value * 4);
+    }
+    else if (kvp.Key == "AY")
+    {
+        totalScore += (kvp.Value * 8);
+    }
+    else if (kvp.Key == "AZ")
+    {
+        totalScore += (kvp.Value * 3);
+    }
+    else if (kvp.Key == "BX")
+    {
+        totalScore += (kvp.Value * 1);
+    }
+    else if (kvp.Key == "BY")
+    {
+        totalScore += (kvp.Value * 5);
+    }
+    else if (kvp.Key == "BZ")
+    {
+        totalScore += (kvp.Value * 9);
+    }
+    else if (kvp.Key == "CX")
+    {
+        totalScore += (kvp.Value * 7);
+    }
+    else if (kvp.Key == "CY")
+    {
+        totalScore += (kvp.Value * 2);
+    }
+    else if (kvp.Key == "CZ")
+    {
+        totalScore += (kvp.Value * 6);
+    }
+}
+
+Console.WriteLine(totalScore); // 10560
+
+watch.Stop();
+Console.WriteLine(watch.ElapsedMilliseconds); // 28ms
