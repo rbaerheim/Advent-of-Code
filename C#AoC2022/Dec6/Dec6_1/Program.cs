7f8ddde182a960c638bbc04272f5dca391e17749
Roger <rgbhm@hotmail.com>
2022-12-06 10:47:18 +0100

    Added dec6

--- C#AoC2022/Dec6/Dec6_1/Program.cs
+++ C#AoC2022/Dec6/Dec6_1/Program.cs
@@ -11,6 +11,8 @@ var path = @"input.txt";
 // Reading lines in input
 var enumLines = File.ReadLines(path, Encoding.UTF8);
 
+char[] testArray = new char[4];
+
 // Total score variable
 int totalScore = 0;
 
@@ -19,11 +21,31 @@ foreach (string line in enumLines)
 {
     // Trim away newlines etc
     line.Trim();
+
+    // Splitting the input characters into a character array
+    char[] charactersInInputArray = line.ToCharArray();
+
+    // Looping though the characters replacing the old ones until I get an array containing distinct characters
+    for (var i = 0; i < charactersInInputArray.Length; i++)
+    {
+        testArray[0] = charactersInInputArray[i];
+        testArray[1] = charactersInInputArray[i + 1];
+        testArray[2] = charactersInInputArray[i + 2];
+        testArray[3] = charactersInInputArray[i + 3];
+
+        if (testArray.Length != testArray.Distinct().Count())
+        {
+            continue;
+        }
+
+        totalScore = (i + 4);
+        break;
+    }
 }
 
 // Prints the totalScore to console
-Console.WriteLine(totalScore);
+Console.WriteLine(totalScore); // 1655
 
 // Stops the benchmarking and prints it to the console.
 watch.Stop();
-Console.WriteLine(watch.ElapsedMilliseconds);
+Console.WriteLine(watch.ElapsedMilliseconds); // 15ms
--- C#AoC2022/Dec6/Dec6_2/Program.cs
+++ C#AoC2022/Dec6/Dec6_2/Program.cs
@@ -11,6 +11,8 @@ var path = @"input.txt";
 // Reading lines in input
 var enumLines = File.ReadLines(path, Encoding.UTF8);
 
+char[] testArray = new char[14];
+
 // Total score variable
 int totalScore = 0;
 
@@ -19,11 +21,42 @@ foreach (string line in enumLines)
 {
     // Trim away newlines etc
     line.Trim();
+
+    // Splitting the input characters into a character array
+    char[] charactersInInputArray = line.ToCharArray();
+
+    // Looping though the characters replacing the old ones until I get an array containing distinct characters
+    for (var i = 0; i < charactersInInputArray.Length; i++)
+    {
+        testArray[0] = charactersInInputArray[i];
+        testArray[1] = charactersInInputArray[i + 1];
+        testArray[2] = charactersInInputArray[i + 2];
+        testArray[3] = charactersInInputArray[i + 3];
+        testArray[4] = charactersInInputArray[i + 4];
+        testArray[5] = charactersInInputArray[i + 5];
+        testArray[6] = charactersInInputArray[i + 6];
+        testArray[7] = charactersInInputArray[i + 7];
+        testArray[8] = charactersInInputArray[i + 8];
+        testArray[9] = charactersInInputArray[i + 9];
+        testArray[10] = charactersInInputArray[i + 10];
+        testArray[11] = charactersInInputArray[i + 11];
+        testArray[12] = charactersInInputArray[i + 12];
+        testArray[13] = charactersInInputArray[i + 13];
+
+
+        if (testArray.Length != testArray.Distinct().Count())
+        {
+            continue;
+        }
+
+        totalScore = (i + 14);
+        break;
+    }
 }
 
 // Prints the totalScore to console
-Console.WriteLine(totalScore);
+Console.WriteLine(totalScore); // 2665
 
 // Stops the benchmarking and prints it to the console.
 watch.Stop();
-Console.WriteLine(watch.ElapsedMilliseconds);
+Console.WriteLine(watch.ElapsedMilliseconds); // 17ms
