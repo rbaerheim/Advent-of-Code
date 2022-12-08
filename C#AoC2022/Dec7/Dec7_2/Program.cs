ed4d451dd7fb24613431630eca60c5b9a71ed05a
rbaerheim <rgbhm@hotmail.com>
2022-12-08 18:11:49 +0100

    Started day 8

--- C#AoC2022/Dec7/Dec7_2/Program.cs
+++ C#AoC2022/Dec7/Dec7_2/Program.cs
@@ -1,2 +1,107 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Benchmarking my code
+        var watch = new Stopwatch();
+        watch.Start();
+
+        // Path for the input
+        var path = @"input.txt";
+
+        // Reading lines in input
+        var enumLines = File.ReadLines(path, Encoding.UTF8);
+
+        // Total score variable
+        int totalScore = 1000000000;
+
+        // Counter for directory ID's.
+        int dirID = 0;
+
+        // Dictionary containing the ID and value of the directories.
+        var dirDictionary = new Dictionary<int, int>();
+
+        // A list containing the ID's of directories currently in use.
+        var treeList = new List<int>();
+
+        // Looping through the input lines
+        foreach (string line in enumLines)
+        {
+            // Trim away newlines etc
+            string[] inputLine = checkFirstWord(line);
+
+            // No need for lines containing listing of items or directories.
+            if (inputLine[1] == "ls" || inputLine[0] == "dir")
+            {
+                continue;
+            }
+
+            // Adding a counter ID to a dictionary or removing the ID if cd ..
+            if (inputLine[1] == "cd")
+            {
+
+                if (inputLine[2] == "..")
+                {
+                    treeList.RemoveAt(treeList.Count() - 1);
+                    continue;
+                }
+                treeList.Add(dirID);
+                dirDictionary[dirID] = 0;
+                dirID += 1;
+                continue;
+            }
+
+            // Check if I can parse the first input value to an integer
+            var isNumeric = int.TryParse(inputLine[0], out int number);
+
+            // If so, I add the integer to all the ID's in the dictionary.
+            if (isNumeric)
+            {
+                foreach (var directory in treeList)
+                {
+                    dirDictionary[directory] += number;
+                }
+                continue;
+            }
+        }
+
+        int neededForUpdate = 30000000;
+        int totalSpaceOnDisk = 70000000;
+        int unUsedSpace = totalSpaceOnDisk - dirDictionary[0];
+        int spaceNeededToBeFreed = neededForUpdate - unUsedSpace;
+
+        Console.WriteLine($"Total space needed for update = {neededForUpdate}");
+        Console.WriteLine($"Total space on Disk = {totalSpaceOnDisk}");
+        Console.WriteLine($"Used space by '/' = {dirDictionary[0]}");
+        Console.WriteLine($"Unused space = {unUsedSpace}");
+        Console.WriteLine($"Space needed to be freed = {spaceNeededToBeFreed}");
+
+        // Looping over the directories, finding the one closest to the size needed to be freed.
+        foreach (KeyValuePair<int, int> kvp in dirDictionary)
+        {
+
+            if (kvp.Value > spaceNeededToBeFreed && kvp.Value < totalScore)
+            {
+                totalScore = kvp.Value;
+            }
+        }
+
+        // Prints the answer to the console
+        Console.WriteLine($"Closest size directory is {totalScore}"); // 7068748
+
+        // Stops the benchmarking and prints it to the console.
+        watch.Stop();
+        Console.WriteLine($"The code took {watch.ElapsedMilliseconds} ms to run"); // 17ms
+
+    }
+
+
+    static string[] checkFirstWord(string line)
+    {
+        var command = line.Trim().Split(" ").ToArray();
+        return command;
+    }
+}
---
+++ C#AoC2022/Dec8/Dec8_1/Program.cs
@@ -0,0 +1,151 @@
+﻿using System.Diagnostics;
+
+// Benchmarking my code
+var watch = new Stopwatch();
+watch.Start();
+
+// Total score variable
+int totalScore = 0;
+
+int rowLength = File.ReadLines(@"input.txt").First().Length;
+int colLength = File.ReadLines(@"input.txt").Count();
+
+// Console.WriteLine(rowLength);
+// Console.WriteLine(colLength);
+
+int topdb;
+int rightdb;
+int bottomdb;
+int leftdb;
+int centerdb;
+
+
+
+int[,] treeArray = new int[rowLength, colLength];
+int min = 10;
+int row = 0;
+int column = 0;
+// int testCounter = 1;
+
+HashSet<string> setOfTreesCoordinates = new HashSet<string>();
+
+// Looping through the input lines
+foreach (string line in System.IO.File.ReadLines(@"input.txt"))
+{
+    column = 0;
+    // Trim away newlines etc
+    char[] charArray = line.Trim().ToCharArray();
+    for (int i = 0; i < charArray.Length; i++)
+    {
+        treeArray[row, column] = (int)Char.GetNumericValue(charArray[i]);
+
+
+        if (row == 0 || column == 0 || column == colLength - 1 || row == rowLength - 1)
+        {
+            setOfTreesCoordinates.Add(row.ToString() + column.ToString());
+        }
+        column += 1;
+    }
+
+    row += 1;
+
+    if (row < rowLength)
+    {
+        continue;
+    }
+
+    Console.WriteLine(setOfTreesCoordinates.Count());
+
+
+    for (int r = 1; r < row; r++)
+    {
+        for (int c = 1; c < column; c++)
+        {
+
+            centerdb = treeArray[r, c];
+            topdb = treeArray[r - 1, c];
+            if (c < 4)
+            {
+                rightdb = treeArray[r, c + 1];
+            }
+            if (r < 4)
+            {
+                bottomdb = treeArray[r + 1, c];
+            }
+            leftdb = treeArray[r, c - 1];
+
+
+            min = 10;
+            for (int left = 0; left < c; left++)
+            {
+                if (treeArray[r, left] < treeArray[r, c] && treeArray[r, left] < min)
+                {
+                    min = treeArray[r, left];
+                    setOfTreesCoordinates.Add(r.ToString() + left.ToString());
+                }
+            }
+            min = 10;
+            for (int right = (rowLength - 1); right > r; right--)
+            {
+                if (treeArray[r, right] < treeArray[r, c] && treeArray[r, right] < min)
+                {
+                    min = treeArray[r, right];
+                    setOfTreesCoordinates.Add(r.ToString() + right.ToString());
+                }
+            }
+            min = 10;
+            for (int top = 0; top < r; top++)
+            {
+                if (treeArray[top, c] < treeArray[r, c] && treeArray[top, c] < min)
+                {
+                    min = treeArray[top, c];
+                    setOfTreesCoordinates.Add(top.ToString() + c.ToString());
+                }
+            }
+            min = 10;
+            for (int bottom = (colLength - 1); bottom > r; bottom--)
+            {
+                if (treeArray[bottom, c] < treeArray[r, c] && treeArray[bottom, r] < min)
+                {
+                    min = treeArray[bottom, r];
+                    setOfTreesCoordinates.Add(bottom.ToString() + c.ToString());
+                }
+            }
+        }
+    }
+
+    /*
+    if (row < 100)
+    {
+        if (testCounter == 10)
+        {
+            break;
+        }
+
+        testCounter += 1;
+        row += 1;
+        if (testCounter == 9)
+        {
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    Console.Write(treeArray[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+        continue;
+    }
+    break;
+    */
+}
+
+// Prints the totalScore to console
+// Console.WriteLine(totalScore);
+
+Console.WriteLine(setOfTreesCoordinates.Count());
+
+// Stops the benchmarking and prints it to the console.
+watch.Stop();
+//Console.WriteLine(watch.ElapsedMilliseconds);
