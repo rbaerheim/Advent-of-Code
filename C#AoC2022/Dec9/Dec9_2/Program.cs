097b3c67feba62e1d78203786684698c505d8f6c
rbaerheim <rgbhm@hotmail.com>
2022-12-09 14:25:58 +0100

    Started dec9

--- C#AoC2022/Dec9/Dec9_2/Program.cs
+++ C#AoC2022/Dec9/Dec9_2/Program.cs
@@ -1,29 +1,24 @@
-﻿using System.Text;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 
 // Benchmarking my code
 var watch = new Stopwatch();
 watch.Start();
 
-// Path for the input
-var path = @"input.txt";
-
-// Reading lines in input
-var enumLines = File.ReadLines(path, Encoding.UTF8);
-
 // Total score variable
 int totalScore = 0;
 
-// Looping through the input lines
-foreach (string line in enumLines)
+// Looping through the input and reading the input lines
+foreach (string line in System.IO.File.ReadLines(@"input.txt"))
 {
     // Trim away newlines etc
     line.Trim();
+    Console.WriteLine(line);
+    break;
 }
 
 // Prints the totalScore to console
-Console.WriteLine(totalScore);
+// Console.WriteLine(totalScore);
 
 // Stops the benchmarking and prints it to the console.
 watch.Stop();
-Console.WriteLine(watch.ElapsedMilliseconds);
+// Console.WriteLine(watch.ElapsedMilliseconds);
