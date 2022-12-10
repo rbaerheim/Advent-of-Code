8ece553f3b3d5e5993483f97817603a09c410321
rbaerheim <rgbhm@hotmail.com>
2022-12-10 18:52:20 +0100

    Added better startcode to dec9_2

--- C#AoC2022/Dec9/Dec9_2/Program.cs
+++ C#AoC2022/Dec9/Dec9_2/Program.cs
@@ -5,20 +5,16 @@ var watch = new Stopwatch();
 watch.Start();
 
 // Total score variable
-int totalScore = 0;
+int solution = 0;
 
 // Looping through the input and reading the input lines
 foreach (string line in System.IO.File.ReadLines(@"input.txt"))
 {
     // Trim away newlines etc
     line.Trim();
-    Console.WriteLine(line);
-    break;
 }
 
-// Prints the totalScore to console
-// Console.WriteLine(totalScore);
+// Stops timer and prints the solution to console and elapsed time to console
+Console.WriteLine($": {solution}, Code runtime: {watch.ElapsedMilliseconds}");
 
-// Stops the benchmarking and prints it to the console.
-watch.Stop();
-// Console.WriteLine(watch.ElapsedMilliseconds);
+// * * 
