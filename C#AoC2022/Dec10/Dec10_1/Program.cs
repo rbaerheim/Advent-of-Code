5919418ea9aa5ae2e14f79fb189d27c459f86e6d
rbaerheim <rgbhm@hotmail.com>
2022-12-10 11:57:57 +0100

    Added startcode for dec10

---
+++ C#AoC2022/Dec10/Dec10_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+
+// Benchmarking my code
+var watch = new Stopwatch();
+watch.Start();
+
+// Total score variable
+int totalScore = 0;
+
+int cycle = 1;
+int x = 1;
+int commandValue;
+
+// Looping through the input and reading the input lines
+foreach (string line in System.IO.File.ReadLines(@"input.txt"))
+{
+    // Trim away newlines etc
+    line.Trim();
+
+    string[] inputLine = line.Split(" ");
+
+    if (cycle == 1 && inputLine.Length == 2)
+    {
+        commandValue = Int32.Parse(inputLine[1]);
+    }
+
+
+    cycle += 1;
+
+}
+
+// Prints the totalScore to console
+Console.WriteLine(totalScore);
+
+// Stops the benchmarking and prints it to the console.
+watch.Stop();
+Console.WriteLine(watch.ElapsedMilliseconds);
---
+++ C#AoC2022/Dec10/Dec10_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+// Benchmarking my code
+var watch = new Stopwatch();
+watch.Start();
+
+// Total score variable
+int totalScore = 0;
+
+// Looping through the input and reading the input lines
+foreach (string line in System.IO.File.ReadLines(@"input.txt"))
+{
+    // Trim away newlines etc
+    line.Trim();
+}
+
+// Prints the totalScore to console
+Console.WriteLine(totalScore);
+
+// Stops the benchmarking and prints it to the console.
+watch.Stop();
+Console.WriteLine(watch.ElapsedMilliseconds);
