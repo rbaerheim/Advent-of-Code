bde61739287b109f4be1fdc01183ef8628224459
rbaerheim <rgbhm@hotmail.com>
2022-12-12 22:17:02 +0100

    Had some fun adding a monkey class and methods, about to start the main functionality

--- C#AoC2022/Dec11/Dec11_1/Monkey.cs
+++ C#AoC2022/Dec11/Dec11_1/Monkey.cs
@@ -1,17 +1,52 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Dec11_1;
 
 class Monkey
 {
-    public int Id { get; set; }
-    public List<int> CurrentItems { get; set; } = new();
-    public string? Operation { get; set; }
-    public string? Test { get; set; }
+    public int Id { get; private set; }
+    public List<int> CurrentItems { get; set; }
+    public string Operation { get; set; }
+    public int Test { get; set; }
+    public int MonkeyTrueId { get; set; }
+    public int MonkeyFalseId { get; set; }
 
-    public Monkey(int id, string operation, string test)
+
+    public Monkey(int id, List<int> currentItems, string operation, int test, int monkeyTrueId, int monkeyFalseId)
     {
         Id = id;
+        CurrentItems = currentItems;
         Operation = operation;
         Test = test;
+        MonkeyTrueId = monkeyTrueId;
+        MonkeyFalseId = monkeyFalseId;
+    }
+
+    public int test(int worryLevel, int divisibleBy)
+    {
+        if (worryLevel % divisibleBy == 0)
+        {
+            return MonkeyTrueId;
+        }
+        return MonkeyFalseId;
+    }
+
+    public int operation(int firstValue, string mathOperator, int secondValue)
+    {
+        switch (mathOperator)
+        {
+            case "+":
+                return firstValue + secondValue;
+            case "-":
+                return firstValue - secondValue;
+            case "*":
+                return firstValue * secondValue;
+            case "/":
+                return firstValue / secondValue;
+            default:
+                return default;
+        }
     }
 
     public void addToCurrentItems(int item)
--- C#AoC2022/Dec11/Dec11_1/Program.cs
+++ C#AoC2022/Dec11/Dec11_1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace Dec11_1;
 
@@ -14,17 +15,42 @@ class Program
         // Total score variable
         int solution = 0;
 
+        string[] monkeyArray = new string[6];
+
+        int forEachMonkey = 0;
+        List<Monkey> monkeys = new List<Monkey>();
+
         // Looping through the input and reading the input lines
         foreach (string line in System.IO.File.ReadLines(@"input.txt"))
         {
+            if (line == "")
+            {
+                continue;
+            }
             // Trim away newlines etc
             line.Trim();
+            monkeyArray[forEachMonkey] = line;
+
+            forEachMonkey += 1;
+
+            if (forEachMonkey == 6)
+            {
+                monkeys.Add(new Monkey(
+                    Int32.Parse(Regex.Match(monkeyArray[0], @"\d+").Value),
+                    monkeyArray[1].Substring(monkeyArray[1].LastIndexOf(':') + 2).Split(",").Select(Int32.Parse).ToList(),
+                    monkeyArray[2].Substring(monkeyArray[2].LastIndexOf('=') + 1),
+                    Int32.Parse(monkeyArray[3].Substring(monkeyArray[3].LastIndexOf(' ') + 1)),
+                    Int32.Parse(Regex.Match(monkeyArray[4], @"\d+").Value),
+                    Int32.Parse(Regex.Match(monkeyArray[5], @"\d+").Value)
+                ));
+                forEachMonkey = 0;
+            }
 
         }
 
         // Stops timer and prints the solution to console and elapsed time to console
         Console.WriteLine($": {solution}, Code runtime: {watch.ElapsedMilliseconds}");
-
+        Console.WriteLine(monkeys.Count());
         // * * 
     }
 }
