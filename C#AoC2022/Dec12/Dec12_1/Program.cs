4d7f2c41204017772970def8f9c4199d6f86c760
rbaerheim <rgbhm@hotmail.com>
2022-12-18 00:10:11 +0100

    Started dec12

--- C#AoC2022/Dec12/Dec12_1/Program.cs
+++ C#AoC2022/Dec12/Dec12_1/Program.cs
@@ -1,20 +1,49 @@
 ﻿using System.Diagnostics;
 
+namespace Dec12_1;
 // Benchmarking my code
-var watch = new Stopwatch();
-watch.Start();
-
-// Total score variable
-int solution = 0;
-
-// Looping through the input and reading the input lines
-foreach (string line in System.IO.File.ReadLines(@"input.txt"))
+class Program
 {
-    // Trim away newlines etc
-    line.Trim();
+    static void Main(string[] args)
+    {
+
+        var watch = new Stopwatch();
+        watch.Start();
+        var input = System.IO.File.ReadLines(@"input.txt");
+        int numOfColumns = input.First().Length;
+        int numOfRows = input.Count();
+
+        char[] characterArray = new char[numOfColumns];
+        int counter = 0;
+        // Total score variable
+        int solution = 0;
+        int[,] map = new int[numOfRows, numOfColumns];
+        // Looping through the input and reading the input lines
+        foreach (string line in input)
+        {
+            // Trim away newlines etc
+            line.Trim();
+            characterArray = line.ToCharArray();
+            for (int i = 0; i < numOfColumns; i++)
+            {
+                switch (characterArray[i])
+                {
+                    case 'S':
+                        map[counter, i] = 100;
+                        break;
+                    case 'E':
+                        map[counter, i] = 1000;
+                        break;
+                    default:
+                        map[counter, i] = char.ToUpper(characterArray[i]) - 64;
+                        break;
+                }
+            }
+            counter += 1;
+            // Stops timer and prints the solution to console and elapsed time to console
+            Console.WriteLine($": {solution}, Code runtime: {watch.ElapsedMilliseconds}");
+        }
+        Console.WriteLine(map[1, 1]);
+        // * * 
+    }
 }
-
-// Stops timer and prints the solution to console and elapsed time to console
-Console.WriteLine($": {solution}, Code runtime: {watch.ElapsedMilliseconds}");
-
-// * * 
