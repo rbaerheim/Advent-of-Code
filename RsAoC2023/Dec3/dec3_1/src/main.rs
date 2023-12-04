38b08f413b01d36eb3ae2bdc1cfcc6d705bf4775
rbaerheim <rgbhm@hotmail.com>
2023-12-04 20:09:37 +0100

    dec3_1

--- RsAoC2023/Dec3/dec3_1/src/main.rs
+++ RsAoC2023/Dec3/dec3_1/src/main.rs
@@ -3,60 +3,67 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
-    let mut result: i32 = 0;
-    let mut i = 0;
+    let mut result: i32 = 0; // 520135
     let mut num: String = String::new();
     let mut matrix: Vec<Vec<char>> = Vec::new();
     let mut keep_number: bool = false;
     if let Ok(input) = read_input_lines("input.txt") {
         for line in input {
             if let Ok(ip) = line {
-                let char_vec: Vec<char> = ip.chars().collect();
-                matrix.push(char_vec);
+                let mut char_vec: Vec<char> = ip.chars().collect();
+                char_vec.insert(0, '.'); // Add '.' at the beginning
+                char_vec.push('.');
+                if matrix.is_empty() {
+                    matrix.push(vec!['.'; char_vec.len()])
+                }
+                matrix.push(char_vec.clone());
+                if matrix.len() == char_vec.len() - 1 {
+                    matrix.push(vec!['.'; char_vec.len()])
+                }
             }
         }
         let mut row_num = matrix.len();
         let mut col_num = matrix[0].len();
 
-        for row in 0..row_num {
-            for col in 0..col_num {
-                while matrix[row][col+i].is_numeric() {
-                    num.push(matrix[row][col+i]);
-                    i = i + 1;
-                    if (row > 0 || col > 0 || row < row_num - 1 || col < col_num - 1) {
-                        if check_adjacent(matrix[row - 1][col - 1]) {
-                            keep_number = true
-                        }
-                        if check_adjacent(matrix[row - 1][col]) {
-                            keep_number = true
-                        }
-                        if check_adjacent(matrix[row - 1][col + 1]) {
-                            keep_number = true
-                        }
-                        if check_adjacent(matrix[row][col - 1]) {
-                            keep_number = true
-                        }
-                        if check_adjacent(matrix[row][col + 1]) {
-                            keep_number = true
-                        }
-                        if check_adjacent(matrix[row + 1][col - 1]) {
-                            keep_number = true
-                        }
-                        if check_adjacent(matrix[row + 1][col]) {
-                            keep_number = true
-                        }
-                        if check_adjacent(matrix[row - 1][col - 1]) {
-                            keep_number = true
-                        }
-                        if check_adjacent(matrix[row + 1][col + 1]) {
-                            keep_number = true
-                        }
+        for row in 1..row_num - 1 {
+            for col in 1..col_num - 1 {
+                if matrix[row][col].is_numeric() {
+                    num.push(matrix[row][col]);
+                    if check_adjacent(matrix[row - 1][col - 1]) {
+                        keep_number = true
+                    }
+                    if check_adjacent(matrix[row + 1][col + 1]) {
+                        keep_number = true
+                    }
+                    if check_adjacent(matrix[row - 1][col + 1]) {
+                        keep_number = true
+                    }
+                    if check_adjacent(matrix[row + 1][col - 1]) {
+                        keep_number = true
+                    }
+                    if check_adjacent(matrix[row - 1][col]) {
+                        keep_number = true
+                    }
+                    if check_adjacent(matrix[row + 1][col]) {
+                        keep_number = true
+                    }
+                    if check_adjacent(matrix[row][col - 1]) {
+                        keep_number = true
+                    }
+                    if check_adjacent(matrix[row][col + 1]) {
+                        keep_number = true
                     }
                 }
-                if !num.is_empty() && keep_number {
-                    result += num.parse::<i32>().unwrap();
-                    num.clear();
+                if !num.is_empty() && keep_number && !matrix[row][col].is_numeric() {
+                    let mut number  = num.parse::<i32>();
+                    match number {
+                        Ok(number) => result += number,
+                        Err(e) => println!("Failed to parse number: {}", e),
+                    }
                     keep_number = false;
+                }
+                if !matrix[row][col].is_numeric() {
+                    num.clear();
                 }
             }
         }
