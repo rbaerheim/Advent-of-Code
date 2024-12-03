b71add0c1963e27b48494f1da786455a5d42bced
Roger <rgbhm@hotmail.com>
2024-12-03 20:23:32 +0100

    Removed helper comments

--- PyAoC2024/Dec3/Dec3_1.py
+++ PyAoC2024/Dec3/Dec3_1.py
@@ -2,14 +2,11 @@ import re
 
 solution = 0
 
-# Read the content of the file
 with open("input.txt", "r") as file:
     text = file.read()
 
-# Regular expression to match 'mul(number,number)'
 pattern = r"mul\((\d+),(\d+)\)"
 
-# Find all matches and convert them into tuples of integers
 matches = re.findall(pattern, text)
 for x, y in matches:
     solution += int(x) * int(y)
