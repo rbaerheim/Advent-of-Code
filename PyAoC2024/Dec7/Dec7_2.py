48b64ad1d396affd3695243d268587d4d62f20ea
Roger <rgbhm@hotmail.com>
2024-12-07 18:29:20 +0100

    Dec_7_1 and 7_2 boilerplate

---
+++ PyAoC2024/Dec7/Dec7_2.py
@@ -0,0 +1,20 @@
+solution = 0
+
+def is_test_value(nums, target, index=1, current_value=None):
+    if current_value is None:
+        current_value = nums[0]
+    if index == len(nums):
+        return current_value == target
+    return (is_test_value(nums, target, index + 1, current_value + nums[index]) or
+            is_test_value(nums, target, index + 1, current_value * nums[index]))
+
+with open('input.txt', 'r', encoding='utf-8-sig') as file:
+    for line in file:
+        line = line.strip()
+        test_value, numbers = line.split(':', 1)
+        test_value = int(test_value)
+        numbers = list(map(int, numbers.split()))
+        if is_test_value(numbers, test_value):
+            solution += test_value
+
+print(solution) # None yet
